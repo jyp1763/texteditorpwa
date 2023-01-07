4061f04e4b4d850e40e67bda3f6f02697bb74475
jyp1763 <108202344+jyp1763@users.noreply.github.com>
2023-01-07 14:26:50 -0500

    initial upload

---
+++ client/src/js/install.js
@@ -0,0 +1,8 @@
+const butInstall = document.getElementById('buttonInstall');
+
+window.addEventListener('beforeinstallprompt', (event) => {});
+
+butInstall.addEventListener('click', async () => {});
+
+window.addEventListener('appinstalled', (event) => {});
+
